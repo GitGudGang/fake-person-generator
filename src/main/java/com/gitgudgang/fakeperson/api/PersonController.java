63753be9a4a6b277ad1253defea20c2eab75e427
R.Ø.M <rasmusorum@gmail.com>
2024-10-21 17:36:16 +0200

    hej

--- src/main/java/com/gitgudgang/fakeperson/api/PersonController.java
+++ src/main/java/com/gitgudgang/fakeperson/api/PersonController.java
@@ -15,12 +15,12 @@ public class PersonController {
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
-    
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/{dtoType}")
     public PartialPersonData getPartialPersonData(@PathVariable String dtoType) {
         return personService.generatePersonData(dtoType);
     }
-
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/bulk")
     public List<FullPersonDTO> getBulkPersons(@RequestParam(defaultValue = "10") int count) {
         return personService.generatePersonList();
