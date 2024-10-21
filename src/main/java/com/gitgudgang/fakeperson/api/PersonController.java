0a0a73be1f15db810a4709482aa70b0452ee9b42
R.Ø.M <rasmusorum@gmail.com>
2024-10-21 20:29:32 +0200

    test

--- src/main/java/com/gitgudgang/fakeperson/api/PersonController.java
+++ src/main/java/com/gitgudgang/fakeperson/api/PersonController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RequestMapping("/api/persons")
 public class PersonController {
     private final PersonService personService;
-    
+    //new
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
